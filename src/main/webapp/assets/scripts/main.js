af10085a029bbba23d5ca7bdfeea8499b36d586c
Rusty Tanton <rusty@perfectsensedigital.com>
2015-06-26 12:34:00 -0400

    removed template test

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -1,5 +1,4 @@
 import bsp_toggle from './plugins/bsp-toggle';
-import require_template_test from './plugins/require-template-test';
 import bsp_carousel_plugin from 'bsp-carousel-plugin';
 import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
 import bsp_share from 'bsp-share';
