7aa88efbc278ad8b7e3559dde677504d95d270d9
Rusty Tanton <rusty@perfectsensedigital.com>
2015-07-22 00:22:52 -0400

    boilerplate bugfix

--- boilerplate/bootstrap.sh
+++ boilerplate/bootstrap.sh
@@ -1,3 +1,3 @@
-curl -s https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/bower.json
-curl -s https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/Gruntfile.js
-curl -s https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/package.json
+curl -O https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/bower.json
+curl -O https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/Gruntfile.js
+curl -O https://raw.githubusercontent.com/perfectsense/brightspot-base/master/boilerplate/package.json
