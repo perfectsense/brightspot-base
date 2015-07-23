fb5f77267686ec3004c726ed8d3f7bbffbed9860
Rusty Tanton <rusty@perfectsensedigital.com>
2015-07-23 10:57:58 -0400

    updated boilerplate dummy spec because it wasn't parsing as es6 be default

--- boilerplate/spec/unit/plugins/delete-me.spec.js
+++ boilerplate/spec/unit/plugins/delete-me.spec.js
@@ -1,3 +1,10 @@
+/**
+ * Spec needs an import statement because systemjs/babel won't
+ * parse this as es6 without one for some reason. It may be there
+ * is a comment flag we could put at the top instead.
+ */
+import $ from 'jquery';
+
 describe('a test spec to make sure the test runner is functioning', () => {
 	it('should run a test spec', () => {
 		expect(true).toBe(true);
