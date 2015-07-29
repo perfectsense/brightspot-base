f8155f52c44ea3c26ae717e3632c3216af2a5f62
Tom Hoppe <trhoppe@gmail.com>
2015-07-29 12:41:35 -0600

    Upping to 0.0.6 to include fixed package.json. Less typos next time

--- boilerplate/bootstrap.sh
+++ boilerplate/bootstrap.sh
@@ -1,5 +1,5 @@
 # variables
-BASE_URL=https://raw.githubusercontent.com/perfectsense/brightspot-base/v0.0.5
+BASE_URL=https://raw.githubusercontent.com/perfectsense/brightspot-base/v0.0.6
 BOILERPLATE_DIR=boilerplate
 SRC_DIR=src/main/webapp
 ASSETS_DIR=$SRC_DIR/assets
