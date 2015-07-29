eb69bfb8fe7528074455a1f1b3d82d853e9e1842
Tom Hoppe <trhoppe@gmail.com>
2015-07-29 12:25:39 -0600

    Removing items out of boilerplate grunt since that stuff comes through bsp-grunt now

--- boilerplate/Gruntfile.js
+++ boilerplate/Gruntfile.js
@@ -21,64 +21,8 @@ module.exports = function(grunt) {
 
             bower: {
 
-                'bootstrap': [
-                    {
-                        cwd: 'less/',
-                        src: '**/*',
-                        dest: 'bower/bootstrap',
-                        expand: true,
-                        flatten: false
-                    },
-                    {
-                        cwd: 'js/',
-                        src: '**/*',
-                        dest: 'bower/bootstrap',
-                        expand: true,
-                        flatten: false
-                    }
-                ],
-
-                'bsp-carousel': [
-                    {
-                        cwd: 'dist/bsp-carousel/',
-                        src: 'bsp-carousel.css',
-                        dest: '../styles/bower/bsp-carousel',
-                        expand: true
-                    },
-                    {
-                        cwd: 'dist/bsp-carousel/',
-                        src: '*.js',
-                        dest: '', //root of scripts
-                        expand: true
-                    },
-                    {
-                        cwd: 'dist/bsp-carousel-thumbnav/',
-                        src: '*.js',
-                        dest: '', //root of scripts
-                        expand: true
-                    },
-                    {
-                        cwd: 'src/less/bsp-carousel-gallery/',
-                        src: '*.less',
-                        dest: '../styles/bower/bsp-carousel-gallery',
-                        expand: true
-                    }
-                ],
-
-                'fontawesome': [
-                    {
-                        src: 'less/*',
-                        dest: '../styles/bower/fontawesome',
-                        expand: true,
-                        flatten: true
-                    },
-                    {
-                        src: 'fonts/*',
-                        dest: '../fonts',
-                        expand: true,
-                        flatten: true
-                    }
-                ], 
+                // history.js and waypoints will go away when brightspot-js-infinite-content gets to be 
+                // it's own separate repo vs being included directly in base
 
                 'history.js' : {
                     src: 'scripts/bundled-uncompressed/html5/native.history.js',
