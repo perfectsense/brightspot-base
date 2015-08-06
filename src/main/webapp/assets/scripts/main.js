40de6c245bb00ade12e225cac2c2f35d90f67b03
Rusty Tanton <rusty@perfectsensedigital.com>
2015-08-06 10:37:18 -0400

    added documentation for js and less base imports

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -1,11 +1,18 @@
-import bsp_tabber_plugin from 'bsp-tabber-plugin';
-import bsp_toggle_plugin from 'bsp-toggle-item-plugin';
-import bsp_carousel_plugin from 'bsp-carousel-plugin';
-import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
-import bsp_carousel_gallery_plugin from './plugins/bsp-carousel-gallery-plugin';
-import bsp_infinite_content_plugin from 'bsp-infinite-content-plugin';
-import bsp_form_plugin from 'bsp-form-plugin';
-import bsp_form_message_plugin from 'bsp-form-message-plugin';
-import bsp_form_constraint_matches_plugin from 'bsp-form-constraint-matches-plugin';
-import bsp_form_field_date_plugin from 'bsp-form-field-date-plugin';
-import bsp_share from 'bsp-share';
+// by default, main is configured to pull in all base libraries
+// many projects will not need all of these libraries, so
+// to optimize you can comment out the base import and just pull
+// in needed libraries
+
+import base from 'base';
+
+// import bsp_tabber_plugin from 'bsp-tabber-plugin';
+// import bsp_toggle_plugin from 'bsp-toggle-item-plugin';
+// import bsp_carousel_plugin from 'bsp-carousel-plugin';
+// import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
+// import bsp_carousel_gallery_plugin from './plugins/bsp-carousel-gallery-plugin';
+// import bsp_infinite_content_plugin from 'bsp-infinite-content-plugin';
+// import bsp_form_plugin from 'bsp-form-plugin';
+// import bsp_form_message_plugin from 'bsp-form-message-plugin';
+// import bsp_form_constraint_matches_plugin from 'bsp-form-constraint-matches-plugin';
+// import bsp_form_field_date_plugin from 'bsp-form-field-date-plugin';
+// import bsp_share from 'bsp-share';
