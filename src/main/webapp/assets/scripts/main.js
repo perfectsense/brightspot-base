b0cc491826435b1f142525737b516220b8114cc9
Tom Hoppe <trhoppe@gmail.com>
2015-10-06 09:43:01 -0600

    Removing base.js and making main.js have all the plugins

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -1,18 +1,27 @@
-// by default, main is configured to pull in all base libraries
-// many projects will not need all of these libraries, so
-// to optimize you can comment out the base import and just pull
-// in needed libraries
+import bsp_ajax_links_plugin from './plugins/bsp-ajax-links-plugin';
 
-import base from 'base';
+import bsp_carousel_plugin from 'bsp-carousel-plugin';
+import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
+import bsp_carousel_gallery_plugin from './plugins/bsp-carousel-gallery-plugin';
 
-// import bsp_toggle_plugin from 'bsp-toggle-item-plugin';
-// import bsp_carousel_plugin from 'bsp-carousel-plugin';
-// import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
-// import bsp_carousel_gallery_plugin from './plugins/bsp-carousel-gallery-plugin';
-// import bsp_infinite_content_plugin from 'bsp-infinite-content-plugin';
-// import bsp_form_plugin from 'bsp-form-plugin';
-// import bsp_form_message_plugin from 'bsp-form-message-plugin';
-// import bsp_form_constraint_matches_plugin from 'bsp-form-constraint-matches-plugin';
-// import bsp_form_field_date_plugin from 'bsp-form-field-date-plugin';
-// import bsp_form_field_datetime_local_plugin from 'bsp-form-field-datetime-local-plugin';
-// import bsp_share from 'bsp-share';
+import bsp_form_plugin from 'bsp-form-plugin';
+import bsp_form_message_plugin from 'bsp-form-message-plugin';
+import bsp_form_constraint_matches_plugin from 'bsp-form-constraint-matches-plugin';
+import bsp_form_field_date_plugin from 'bsp-form-field-date-plugin';
+import bsp_form_field_datetime_local_plugin from 'bsp-form-field-datetime-local-plugin';
+import bsp_form_field_month_plugin from 'bsp-form-field-month-plugin';
+import bsp_form_field_time_plugin from 'bsp-form-field-time-plugin';
+
+import bsp_infinite_content_plugin from 'bsp-infinite-content-plugin';
+import bsp_lazyimage_plugin from './plugins/bsp-lazyimage-plugin';
+import bsp_scroll_checker from './plugins/bsp-scroll-checker-plugin';
+import bsp_share from 'bsp-share';
+import bsp_tabber_plugin from 'bsp-tabber-plugin';
+import bsp_toggle_plugin from 'bsp-toggle-item-plugin';
+import bsp_modal_plugin from './plugins/bsp-modal-plugin';
+
+// need to find a neater method than compiling this in our production JS
+// TODO: we are moving to bsp-template for Node, so this will not be needed anymore
+import bsp_template_plugin from 'bsp-template-plugin';
+
+export default {};
