7a70933d7bb767417f3c2b632c538bdabaecd81b
Tom Hoppe <trhoppe@gmail.com>
2015-10-20 09:54:37 -0600

    Moving gallery code to it's own repo. Pulling that stuff in via bsp-grunt task vs separate bower tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,10 @@ module.exports = function(grunt) {
 
     require('bsp-grunt')(grunt, {
         bsp: {
+            brightspotBase: {
+                enable: true
+            },
+
             styles: {
                 dir: 'assets/styles',
                 less: [
@@ -14,102 +18,6 @@ module.exports = function(grunt) {
 
             scripts: {
                 dir: 'assets/scripts'
-            },
-
-            bower: {
-
-                'bootstrap': [
-                    {
-                        cwd: 'less/',
-                        src: '**/*',
-                        dest: 'bower/bootstrap',
-                        expand: true,
-                        flatten: false
-                    },
-                    {
-                        cwd: 'js/',
-                        src: '**/*',
-                        dest: 'bower/bootstrap',
-                        expand: true,
-                        flatten: false
-                    }
-                ],
-
-                'vex': [
-                    {
-                        cwd: 'js/',
-                        src: '**/*.js',
-                        dest: 'bower/vex',
-                        expand: true,
-                        flatten: false
-                    }
-                ],
-
-                'bsp-carousel': [
-                    {
-                        cwd: 'dist/bsp-carousel/',
-                        src: 'bsp-carousel.css',
-                        dest: '../styles/bower/bsp-carousel',
-                        expand: true
-                    },
-                    {
-                        cwd: 'dist/bsp-carousel/',
-                        src: '*.js',
-                        dest: '', //root of scripts
-                        expand: true
-                    },
-                    {
-                        cwd: 'dist/bsp-carousel-thumbnav/',
-                        src: '*.js',
-                        dest: '', //root of scripts
-                        expand: true
-                    },
-                    {
-                        cwd: 'src/less/bsp-carousel-gallery/',
-                        src: '*.less',
-                        dest: '../styles/bower/bsp-carousel-gallery',
-                        expand: true
-                    }
-                ],
-
-                'bsp-tabber': [
-                    {
-                        src: 'src/css/bsp-tabber.css',
-                        dest: '../styles/bower/bsp-tabber.css'
-                    },
-                    {
-                        cwd: 'src/js/',
-                        src: '*.js',
-                        dest: '',
-                        expand: true
-                    }
-                ],
-
-                'datetimepicker': [
-                    {
-                        src: 'jquery.datetimepicker.js',
-                        dest: 'jquery.datetimepicker.js'
-                    },
-                    {
-                        src: 'jquery.datetimepicker.css',
-                        dest: '../styles/bower/jquery.datetimepicker.css'
-                    }
-                ],
-
-                'fontawesome': [
-                    {
-                        src: 'less/*',
-                        dest: '../styles/bower/fontawesome',
-                        expand: true,
-                        flatten: true
-                    },
-                    {
-                        src: 'fonts/*',
-                        dest: '../fonts',
-                        expand: true,
-                        flatten: true
-                    }
-                ]
             }
         }
 
--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -2,7 +2,7 @@ import bsp_ajax_links_plugin from './plugins/bsp-ajax-links-plugin';
 
 import bsp_carousel_plugin from 'bsp-carousel-plugin';
 import bsp_carousel_thumbnav_plugin from 'bsp-carousel-thumbnav-plugin';
-import bsp_carousel_gallery_plugin from './plugins/bsp-carousel-gallery-plugin';
+import bsp_carousel_gallery_plugin from 'bsp-carousel-gallery-plugin';
 
 import bsp_form_plugin from 'bsp-form-plugin';
 import bsp_form_message_plugin from 'bsp-form-message-plugin';
