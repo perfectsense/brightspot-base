d65e58aa896cded6b1ed3391393883eb118a6a00
Hyoo Lim <hyoolim@gmail.com>
2015-10-22 14:00:21 -0400

    Changes from @jpencola that allows serving example JSON files in styleguide directly.

--- src/main/node/brightspot-fe-dev-server.js
+++ src/main/node/brightspot-fe-dev-server.js
@@ -1,95 +1,180 @@
+var Promise = global.Promise || require('promise');
+var log = require('./logger');
 var _ = require('lodash');
 var express = require('express');
 var fs = require('fs');
+var cors = require('cors');
+var favicon = require('serve-favicon');
+var hbs = require('handlebars');
 var parser = require('xml2json');
 var path = require('path');
-var targetNameFromPomXml = function(file) {
-	var xml = parser.toJson( fs.readFileSync(file), { object: true } );
-	return xml.project.artifactId + '-' + xml.project.version;
-};
+var hbsRenderer = require('./handlebars-renderer');
+var dataGenerator = require('./data-generator');
+var serverConfig;
+
 var defaults = {
-	brightSpotBaseRelPath: 'node_modules/brightspot-base',
-	pom: 'pom.xml',
-	wwwroot: 'styleguide',
-	srcRelPath: 'src/main/webapp',
-	host: 'localhost',
-	port: 3000
+    brightSpotBaseRelPath: 'node_modules/brightspot-base',
+    pom: 'pom.xml',
+    wwwroot: 'styleguide',
+    srcRelPath: 'src/main/webapp',
+    host: 'localhost',
+    port: 3000
 };
+
+var targetNameFromPomXml = function(file) {
+  var xml = parser.toJson( fs.readFileSync(file), { object: true } );
+  return xml.project.artifactId + '-' + xml.project.version;
+};
+
+var _prepareResponse = function(req, res, next) {
+    var dataFileUri;
+
+    // if the originalUrl ends with a "/" then default to loading the "index.json" file.
+    // eg: localhost:3000/pages/example/
+    if (req.originalUrl.slice(-1) === '/'){
+        dataFileUri = req.originalUrl + 'index.json';
+    }
+    // if the request has an ".html" extension then just return the response with no additional processing
+    else if (req.originalUrl.slice(-5) === '.html') {
+        next();
+    }
+    // otherwise, assume the user wants a data file and just append the ".json" extension
+    else {
+        dataFileUri = req.baseUrl + '.json';
+    }
+
+    // First, get the entrypoint template as a string
+    hbsRenderer.getTemplateAsString('main.hbs')
+        .then(function(templateString){
+            // Then, get the entrypoint data as JSON
+            return hbsRenderer.getJSONData(dataFileUri)
+                .then(function(jsonData){
+                    // create a viewmodel out of the JSON
+                    var vm = hbsRenderer.createViewModel(jsonData);
+                    // compile the template
+                    var template = hbs.compile(templateString, {noEscape: true});
+                    // postprocess the viewmodel data
+                    dataGenerator.process(vm)
+                        // then, hydrate the template and return to client
+                        .then(function(value){
+                            return res.send(template(value));
+                        });
+                });
+        });
+};
+
 module.exports = {
-	listen: function(config) {
-		var app = express();
-		var target;
+  listen: function(config) {
+    var app = express();
+    var target;
 
-		console.log('BRIGHTSPOT FRONT END DEVELOPMENT SERVER');
-		console.log('=======================================');
+        log.welcome();
 
-		// merge config with defaults
-		config = _.extend({}, defaults, config);
+        app.set('env', 'development');
+        app.use(cors());
 
-		// check project dir
-		if (!fs.existsSync(config.projectDir)) {
-			console.error('ERROR: Project directory %s not specified or does not exist', config.projectDir);
-			process.exit(1);
-		}
+    // merge config with defaults
+    config = _.extend({}, defaults, config);
 
-		// configure/check target path
-		config.targetPath = config.projectDir + '/target/' + targetNameFromPomXml( config.projectDir + '/' + config.pom );
-		if (!fs.existsSync(config.targetPath)) {
-			console.error('ERROR: Target dir %s does not exist', config.targetRelPath);
-			process.exit(1);
-		}
+    // check project dir
+    if (!fs.existsSync(config.projectDir)) {
+      console.error('ERROR: Project directory %s not specified or does not exist', config.projectDir);
+      process.exit(1);
+    }
 
-		// configure/check brightspot base path
-		config.brightspotBasePath = config.projectDir + '/' + config.brightSpotBaseRelPath + '/' + config.wwwroot;
-		if (!fs.existsSync(config.brightspotBasePath)) {
-			console.error('WARNING: Brightspot Base dir %s does not exist', config.brightspotBasePath);
-		}
+    // configure/check target path
+    config.targetPath = config.projectDir + '/target/' + targetNameFromPomXml( config.projectDir + '/' + config.pom );
+    if (!fs.existsSync(config.targetPath)) {
+      console.error('ERROR: Target dir %s does not exist', config.targetRelPath);
+      process.exit(1);
+    }
 
-		// configure/check project server root
-		config.projectServerRoot = config.projectDir + '/' + config.wwwroot;
-		if (!fs.existsSync(config.projectServerRoot)) {
-			console.error('ERROR: Styleguide dir %s does not exist', config.projectServerRoot);
-			process.exit(1);
-		}
+    // configure/check brightspot base path
+    config.brightspotBasePath = config.projectDir + '/' + config.brightSpotBaseRelPath + '/' + config.wwwroot;
+    if (!fs.existsSync(config.brightspotBasePath)) {
+      console.error('WARNING: Brightspot Base dir %s does not exist', config.brightspotBasePath);
+    }
 
-		// server root looks for files in project first, then in brightspot base
-		if (fs.existsSync(config.projectServerRoot)) {
-			app.use( express.static(config.projectServerRoot) );
-			console.log('Serving / from %s', config.projectServerRoot);
-		} else {
-			console.log('WARNING: Path %s does not exist', config.projectServerRoot);
-		}
-		
-		if (fs.existsSync(config.brightspotBasePath)) {
-			app.use( express.static(config.brightspotBasePath) );
-			console.log('Serving / from %s', config.brightspotBasePath);
-		} else {
-			console.log('WARNING: Path %s does not exist', config.brightspotBasePath);
-		}
+    // configure/check project server root
+    config.projectServerRoot = config.projectDir + '/' + config.wwwroot;
+    if (!fs.existsSync(config.projectServerRoot)) {
+      console.error('ERROR: Styleguide dir %s does not exist', config.projectServerRoot);
+      process.exit(1);
+    }
 
-		// assets/render looks for file in project first, then in target
-		var serverPaths = [
-			{ '/assets': config.projectDir + '/' + config.srcRelPath + '/assets' },
-			{ '/assets': config.targetPath + '/assets' },
-			{ '/assets': config.projectDir + '/' + config.brightSpotBaseRelPath  + '/' + config.srcRelPath + '/assets' },
-			{ '/render': config.projectDir + '/' + config.srcRelPath + '/render' },
-			{ '/render': config.targetPath + '/render' },
-			{ '/render': config.projectDir + '/' + config.brightSpotBaseRelPath  + '/' + config.srcRelPath + '/render' }
-		];
-		_.forEach(serverPaths, function(value) {
-			_.forEach(value, function(localPath, serverPath) {
-				if (fs.existsSync(localPath)) {
-					console.log('Serving %s from %s', serverPath, localPath);
-					app.use( serverPath, express.static(localPath) );
-				} else {
-					console.log('WARNING: Path %s does not exist', localPath);
-				}
-			});
-		});
+    // server root looks for files in project first, then in brightspot base
+    if (fs.existsSync(config.projectServerRoot)) {
+      app.use( express.static(config.projectServerRoot) );
+      console.log('Serving / from %s', config.projectServerRoot);
+    } else {
+      console.log('WARNING: Path %s does not exist', config.projectServerRoot);
+    }
 
-		// start the server
-		app.listen(config.port, config.host, function() {
-			console.log('Listening on %s:%s', config.host, config.port);
-		});
-	}
+    if (fs.existsSync(config.brightspotBasePath)) {
+      app.use( express.static(config.brightspotBasePath) );
+      console.log('Serving / from %s', config.brightspotBasePath);
+    } else {
+      console.log('WARNING: Path %s does not exist', config.brightspotBasePath);
+    }
+
+        log.success(config.projectDir);
+        log.success(config.targetPath);
+        log.success(config.brightspotBasePath);
+        log.success(config.projectServerRoot);
+        log.success(config.srcRelPath);
+
+        // TemplatePaths are a list of relative paths to template directories.
+        // Order matters; where the first item has the highest precedence for the application to serve from
+        config.templatePaths = [
+            config.projectDir + '/' + config.srcRelPath + '/render',
+            config.targetPath + '/render',
+            config.projectDir + '/' + config.brightSpotBaseRelPath  + '/' + config.srcRelPath + '/render'
+        ];
+
+        // DataPaths are a list of relative paths to data/json directories
+        // Order matters; where the first item has the highest precedence for the application to serve from
+        config.dataPaths = [
+            config.projectServerRoot,
+            config.projectDir + '/' + config.brightSpotBaseRelPath + '/' + config.wwwroot
+        ];
+
+    // assets/render looks for file in project first, then in target
+    var serverPaths = [
+      { '/assets': config.projectDir + '/' + config.srcRelPath + '/assets' },
+      { '/assets': config.targetPath + '/assets' },
+      { '/assets': config.projectDir + '/' + config.brightSpotBaseRelPath  + '/' + config.srcRelPath + '/assets' },
+      { '/render': config.projectDir + '/' + config.srcRelPath + '/render' },
+      { '/render': config.targetPath + '/render' },
+      { '/render': config.projectDir + '/' + config.brightSpotBaseRelPath  + '/' + config.srcRelPath + '/render' }
+    ];
+
+        // define all the static paths to serve from
+    _.forEach(serverPaths, function(value) {
+      _.forEach(value, function(localPath, serverPath) {
+        if (fs.existsSync(localPath)) {
+          console.log('Serving %s from %s', serverPath, localPath);
+          app.use( serverPath, express.static(localPath) );
+        } else {
+          console.log('WARNING: Path %s does not exist', localPath);
+        }
+      });
+    });
+
+        // Init the server-side template renderer
+        hbsRenderer.init(config);
+
+        // serves the Favicon
+        //app.use(favicon(config.projectDir + '/' + config.srcRelPath + '/assets/images/favicon.ico'));
+
+        // After app.use() is configured above for the static asset paths from the filesystem,
+        // we can handle all other requests and pass them through our custom middleware which renders the HBS templates.
+        app.use('*', function(req, res, next) {
+            _prepareResponse(req, res, next);
+        });
+
+    // start the server
+    app.listen(config.port, config.host, function() {
+      console.log('Listening on %s:%s', config.host, config.port);
+    });
+  }
 };
