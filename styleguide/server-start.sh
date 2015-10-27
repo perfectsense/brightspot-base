4a964dd4e7921ea9a7e4bc5f29dbf09da79afccc
Hyoo Lim <hyoolim@gmail.com>
2015-10-27 18:30:12 -0400

    Moves styleguide server into its own project at brightspot-js-styleguide.

--- styleguide/server-start.sh
+++
@@ -1,6 +0,0 @@
-NODE=$( dirname .. )/node/node
-SERVER=$( dirname "${BASH_SOURCE[0]}" )/server.js
-$NODE $SERVER "$@" &>/dev/null &
-SERVER_PID=$!
-echo $SERVER_PID > $( dirname "${BASH_SOURCE[0]}" )/server.manifest
-echo "Server started"
