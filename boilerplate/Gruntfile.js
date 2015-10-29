057feda387357a93421aafb50210ff6d08824b63
Tom Hoppe <trhoppe@gmail.com>
2015-10-29 16:22:03 -0400

    Starting to clean up boilerplate

--- boilerplate/Gruntfile.js
+++ boilerplate/Gruntfile.js
@@ -7,66 +7,17 @@ module.exports = function(grunt) {
             brightspotBase: {
                 enable: true
             },
+
             styles: {
                 dir: 'assets/styles',
                 less: [
                     ' *.less'
                 ],
-                autoprefixer: false
+                autoprefixer: true
             },
 
             scripts: {
                 dir: 'assets/scripts'
-            },
-
-            bower: {
-
-                'bsp-tabber': [
-                    {
-                        src: 'src/css/bsp-tabber.css',
-                        dest: '../styles/bower/bsp-tabber.css'
-                    },
-                    {
-                        cwd: 'src/js/',
-                        src: '*.js',
-                        dest: '',
-                        expand: true
-                    }
-                ],
-
-                'datetimepicker': [
-                    {
-                        src: 'jquery.datetimepicker.js',
-                        dest: 'jquery.datetimepicker.js'
-                    },
-                    {
-                        src: 'jquery.datetimepicker.css',
-                        dest: '../styles/bower/jquery.datetimepicker.css'
-                    }
-                ],
-
-                'vex': [
-                    {
-                        cwd: 'js/',
-                        src: '**/*.js',
-                        dest: 'bower/vex',
-                        expand: true,
-                        flatten: false
-                    }
-                ]
-
-                // Here you can specify extra bower packages that dont come as part of base if you need them
-                // if you add something into bower.json and do NOT specify in here, it will just copy whatever is
-                // in that packages' bower.json main entry. Otherwise, you can grab other types of files here
-
-                // example
-                //'history.js' : {
-                //    src: 'scripts/bundled-uncompressed/html5/native.history.js',
-                //    dest: './',
-                //    expand: true,
-                //    flatten: true
-                //}
-
             }
         }
 
