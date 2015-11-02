b94df4ecc3077bc79daaafc98354f0d380fd0d63
Tom Hoppe <trhoppe@gmail.com>
2015-11-02 13:29:28 -0700

    Change to main.js for ajax links

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -1,6 +1,7 @@
 /**
  * Our main JS file which is used to compile the JS by including any plugins that are used throughout the project
- * Base doesn't have any JS in it, but rather any JS plugins that are used in base are copied for you with bower
+ * Base has as little JS in it as possible. We try to use reusable JS plugins that have their own repos and can be
+ * used across multiple projects. We then compile them in, by bringing them in via bower and they get copied
  * via the bsp-grunt task from their respective repos and compiled in with this file.
  *
  * If you have any project specific JS that you need to write, put it into your own scripts/plugins folder
@@ -9,6 +10,7 @@
  * If you are going to use any plugins that don't come with base and base components, designate them in your
  * own bower.json and copy them into the scripts/bower folder via a Gruntfile entry
  */
+import bsp_ajax_links_plugin from           'bsp-ajax-links-plugin';
 import bsp_carousel_plugin from             'bsp-carousel-plugin';
 import bsp_carousel_thumbnav_plugin from    'bsp-carousel-thumbnav-plugin';
 import bsp_carousel_gallery_plugin from     'bsp-carousel-gallery-plugin';
@@ -18,12 +20,18 @@ import bsp_share from                       'bsp-share';
 import bsp_tabber_plugin from               'bsp-tabber-plugin';
 import bsp_toggle_plugin from               'bsp-toggle-item-plugin';
 
+
+/**
+ * These are Base specific JS plugins. These are not generic enough to have their own repo, so we put them in the base plugins
+ * folder, and they can be compiled in, if you need to use them
+ */
+import base_scroll_checker from              'base/plugins/base-scroll-checker-plugin';
+
+
 /**
  * These files here are currently residing in base in the plugins folder for ease of development, until we get them
  * finalized and to a version 1.0 and put them in their own repos.
  */
-import bsp_ajax_links_plugin from           'base/plugins/bsp-ajax-links-plugin';
 import bsp_modal_plugin from                'base/plugins/bsp-modal-plugin';
-import bsp_scroll_checker from              'base/plugins/bsp-scroll-checker-plugin';
 import bsp_table_sort_plugin from           'base/plugins/bsp-table-sort-plugin';
 export default {};
