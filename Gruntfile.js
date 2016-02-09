5d5083c58f3f4f066b1f736d2da6e059e0513ae7
John Pencola <paulpencola@gmail.com>
2016-02-09 17:10:32 -0500

    Add normalize.css to build pipeline via Bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,16 @@
 module.exports = function (grunt) {
     require('bsp-grunt')(grunt, {
         bsp: {
+
+            bower: {
+                'normalize-css': [
+                    {
+                        src: 'normalize.css',
+                        dest: 'bower/normalize-css/normalize.css'
+                    }
+                ]
+            },
+
             styles: {
                 dir: 'assets',
                 less: [ '*.less' ],
