5327b9226a6db0c5dee49501daa2368e62986613
John Pencola <paulpencola@gmail.com>
2016-08-11 12:56:31 -0400

    Add back in deleted files from merging release/3.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,46 @@ module.exports = function (grunt) {
     require('bsp-grunt')(grunt, {
         bsp: {
 
+            bower: {
+                // Gallery uses Masonry javascript, which has a few dependencies
+                // BEGIN dependencies for Masonry
+                'outlayer': [
+                    {
+                        src: 'outlayer.js',
+                        dest: 'outlayer/outlayer.js'
+                    },
+                    {
+                        src: 'item.js',
+                        dest: 'outlayer/item.js'
+                    }
+                ],
+                'ev-emitter': [
+                    {
+                        src: 'ev-emitter.js',
+                        dest: 'ev-emitter/ev-emitter.js'
+                    }
+                ],
+                'get-size': [
+                    {
+                        src: 'get-size.js',
+                        dest: 'get-size/get-size.js'
+                    }
+                ],
+                'fizzy-ui-utils': [
+                    {
+                        src: 'utils.js',
+                        dest: 'fizzy-ui-utils/utils.js'
+                    }
+                ],
+                'desandro-matches-selector': [
+                    {
+                        src: 'matches-selector.js',
+                        dest: 'desandro-matches-selector/matches-selector.js'
+                    }
+                ]
+                // END dependencies for Masonry
+            },
+
             styles: {
                 dir: '',
                 less: [
