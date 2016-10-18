897327a374996ded722bd888307cd600284eaeab
Tom Hoppe <trhoppe@gmail.com>
2016-10-18 14:58:21 -0600

    Documentation and new "copy-bower" task

--- styleguide/gulpfile.js
+++ styleguide/gulpfile.js
@@ -1,36 +1,42 @@
 const gulp = require('gulp');
 const styleguide = require('brightspot-styleguide/tasks/all.js');
 
+// the clean styleguide tasks blanks out the _build and _dist directories
 gulp.task('clean', function () {
     return styleguide.clean()
 });
 
-gulp.task('bower', ['clean'], function (cb) {
-    // TODO: be more literal with what is being installed
-    // .installBowerComponent('jquery', '[...]')
-    return styleguide.bower()
+// simple bower install that goes through your bower.json and brings those components down into the _build/bower_components folder
+gulp.task('bower-install', ['clean'], function () {
+    return styleguide.bowerInstall();
 });
 
-gulp.task('copy-bower-components', ['bower'], function (cb) {
-    return gulp.src([
-            '_build/bower_components/normalize-css/normalize.css'
-        ])
-        .pipe(gulp.dest('_build'))
+// this task gathers all the js, less, and css from this folders and copies it to _build so we can do our compilation there
+gulp.task('copy-src', ['clean'], function() {
+    return styleguide.copySrc()
 });
 
-gulp.task('copy-src-to-temp', ['clean'], function() {
-    return gulp.src(['!_build/**', './**/*.less'])
-        .pipe(gulp.dest('./_build/'));
+// the copyBower styleguide task goes through the main entries in your bower.json dependencies and copies those files
+// from those components to the root of _build for compiling. If you want to override what to copy in from your bowered component,
+// you can create a "overrides" object with alternate main entries to copy those items into the root of _build instead/
+// If you need to copy to somewhere that is NOT root, add an ignore: true to the override for that bower component
+// and copy manually like the example below
+gulp.task('copy-bower', ['bower-install'], function () {
+    return styleguide.copyBower();
 });
+// gulp.task('copy-custom-bower-files', ['bower-install'], function (cb) {
+//     return  gulp.src('./_build/bower_components/my-bowered-in-component')
+//                 .pipe(gulp.dest('./_dist/custom-directory-for-bowered-in-component'))
+// });
 
-gulp.task('less', ['copy-src-to-temp', 'copy-bower-components'], function() {
+gulp.task('less', ['copy-src', 'copy-bower'], function() {
     return gulp.src('_build/All.less')
         .pipe(styleguide.compileStyles())
         .pipe(gulp.dest('./_dist'))
 });
 
-gulp.task('systemjs', ['clean'], function (cb) {
-    return gulp.src('All.js')
+gulp.task('systemjs', ['copy-src', 'copy-bower'], function (cb) {
+    return gulp.src('_build/All.js')
         // commenting out until we actually have something that will work for compileScripts
         //.pipe(styleguide.compileScripts());
         .pipe(gulp.dest('./_dist'))
