093dd295fbda909bc30a8af08620f403c2ccf883
John Pencola <paulpencola@gmail.com>
2016-10-18 11:57:51 -0400

    Add compile task with initial call to compileScripts task in styleguide

--- styleguide/gulpfile.js
+++ styleguide/gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp')
 const styleguide = require('brightspot-styleguide/tasks/all.js')
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     return styleguide.clean(['_dist/**/*', '_tmp/**/*'])
 })
 
-gulp.task('install', ['clean'], function(cb) {
+gulp.task('install', ['clean'], function (cb) {
+    // TODO: be more literal with what is being installed
+    // .installBowerComponent('jquery', '[...]')
     return styleguide.install()
 })
 
-gulp.task('default', ['install'], function(cb) {
+gulp.task('compile', ['install'], function (cb) {
+    gulp.src('All.js')
+        .pipe(styleguide.compileScripts())
+})
+
+gulp.task('default', ['compile'], function (cb) {
     return
 })
 
