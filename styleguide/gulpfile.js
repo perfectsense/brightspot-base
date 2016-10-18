a1f25a93ce0d03701c5bfdd4db496790a9d5dece
Tom Hoppe <trhoppe@gmail.com>
2016-10-18 12:50:30 -0600

    Added copy and less tasks

--- styleguide/gulpfile.js
+++ styleguide/gulpfile.js
@@ -1,23 +1,43 @@
-const gulp = require('gulp')
-const styleguide = require('brightspot-styleguide/tasks/all.js')
+const gulp = require('gulp');
+const styleguide = require('brightspot-styleguide/tasks/all.js');
 
 gulp.task('clean', function () {
-    return styleguide.clean(['_dist/**/*', '_tmp/**/*'])
-})
+    return styleguide.clean()
+});
 
-gulp.task('install', ['clean'], function (cb) {
+gulp.task('bower', ['clean'], function (cb) {
     // TODO: be more literal with what is being installed
     // .installBowerComponent('jquery', '[...]')
-    return styleguide.install()
-})
+    return styleguide.bower()
+});
 
-gulp.task('compile', ['install'], function (cb) {
-    gulp.src('All.js')
-        .pipe(styleguide.compileScripts())
-})
+gulp.task('copy-bower-components', ['bower'], function (cb) {
+    return gulp.src([
+            '_build/bower_components/normalize-css/normalize.css'
+        ])
+        .pipe(gulp.dest('_build'))
+});
 
-gulp.task('default', ['compile'], function (cb) {
+gulp.task('copy-src-to-temp', ['clean'], function() {
+    return gulp.src(['!_build/**', './**/*.less'])
+        .pipe(gulp.dest('./_build/'));
+});
+
+gulp.task('less', ['copy-src-to-temp', 'copy-bower-components'], function() {
+    return gulp.src('_build/All.less')
+        .pipe(styleguide.compileStyles())
+        .pipe(gulp.dest('./_dist'))
+});
+
+gulp.task('systemjs', ['clean'], function (cb) {
+    return gulp.src('All.js')
+        // commenting out until we actually have something that will work for compileScripts
+        //.pipe(styleguide.compileScripts());
+        .pipe(gulp.dest('./_dist'))
+});
+
+gulp.task('default', ['less', 'systemjs'], function (cb) {
     return
-})
+});
 
-module.exports = gulp
+module.exports = gulp;
