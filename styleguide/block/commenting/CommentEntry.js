e53e22bbf9e0192ab343145d96a4bfd9984288e5
John Pencola <paulpencola@gmail.com>
2016-11-02 15:00:35 -0400

    Update source to conform to standard formatting rules

--- styleguide/block/commenting/CommentEntry.js
+++ styleguide/block/commenting/CommentEntry.js
@@ -1,122 +1,121 @@
-import $ from 'jquery';
-import bspUtils from 'bsp-utils';
+import $ from 'jquery'
+import bspUtils from 'bsp-utils'
 
 class CommentEntry {
-    constructor($context, options) {
-        this.$context = $context;
-        this.settings = $.extend({}, {
-            selectors: {
-                prefix: '.CommentEntry',
-                commentBlock: '.Comment',
-                commentingBlock: '.Commenting',
-                signInBlock: '.UserSignIn',
-                validationBlock: '.ValidationMessages',
-                responseBlock: '.CommentActionResponse'
-            }
-        }, options);
+  constructor ($context, options) {
+    this.$context = $context
+    this.settings = $.extend({}, {
+      selectors: {
+        prefix: '.CommentEntry',
+        commentBlock: '.Comment',
+        commentingBlock: '.Commenting',
+        signInBlock: '.UserSignIn',
+        validationBlock: '.ValidationMessages',
+        responseBlock: '.CommentActionResponse'
+      }
+    }, options)
 
-        this.commentingId = this.$context.attr('data-commenting-id') ||
-                            this.$context.parents('.Commenting').attr('id');
+    this.commentingId = this.$context.attr('data-commenting-id') ||
+                            this.$context.parents('.Commenting').attr('id')
 
-        this.$form.submit((event)=> {
-            event.preventDefault();
-            this.onSubmit();
-            this.$context.trigger('CommentEntry:onSubmitComment');
-        });
-    }
+    this.$form.submit((event) => {
+      event.preventDefault()
+      this.onSubmit()
+      this.$context.trigger('CommentEntry:onSubmitComment')
+    })
+  }
 
-    get selectors() {
-        return this.settings.selectors;
-    }
+  get selectors () {
+    return this.settings.selectors
+  }
 
-    get $submitButton() {
-        return this.$context.find(`${this.selectors.prefix}-submit-button`);
-    }
+  get $submitButton () {
+    return this.$context.find(`${this.selectors.prefix}-submit-button`)
+  }
 
-    get $form() {
-        return this.$context.find(`${this.selectors.prefix}-form`);
-    }
+  get $form () {
+    return this.$context.find(`${this.selectors.prefix}-form`)
+  }
 
-    onSubmit() {
-        $.ajax({
-            url: this.$form.attr('action'),
-            method: this.$form.attr('method'),
-            data: this.$form.serialize()
+  onSubmit () {
+    $.ajax({
+      url: this.$form.attr('action'),
+      method: this.$form.attr('method'),
+      data: this.$form.serialize()
+    })
+        .done((response) => {
+          this.render(response)
         })
-        .done((response)=> {
-            this.render(response);
+        .fail((data) => {
+          this.onRequestError(data)
         })
-        .fail((data)=> {
-            this.onRequestError(data);
-        });
+  }
+
+  onRequestError (data) {
+    let $errorMessage = this.$context.find(`${this.selectors.validationBlock}-serverError`)
+    $errorMessage.attr('data-visible', '')
+    this.$context.trigger('CommentEntry:onSubmitCommentError', { error: data })
+  }
+
+  render (data) {
+    let $html = $(data)
+    let $comment = $html.find(this.selectors.commentBlock)
+    let $title = $html.find(`${this.selectors.responseBlock}-title`)
+    let $commentEntry = $html.find(this.selectors.prefix)
+    let $signIn = $html.find(this.selectors.signInBlock)
+    let replaceWithResponse = true
+
+    // render a comment?
+    if ($comment.length > 0) {
+      $html = $comment
+
+      // if this block is not a child of a commenting block
+      // or is rendered within the replies section, we should replace it with the response
+      replaceWithResponse = this.$context.parents('.Commenting').length === 0 ||
+                            this.$context.parents('.Comment-replies').length > 0
+
+      this.$context.trigger('CommentEntry:onSubmitCommentSuccess', {
+        commentingId: this.commentingId,
+        $comment: $comment,
+        $title: $title,
+        alreadyRendered: replaceWithResponse
+      })
+
+      this.reset()
     }
 
-    onRequestError(data) {
-        let $errorMessage = this.$context.find(`${this.selectors.validationBlock}-serverError`);
-        $errorMessage.attr('data-visible', '');
-        this.$context.trigger('CommentEntry:onSubmitCommentError', { error: data });
+    // render comment entry form?
+    if ($commentEntry.length > 0) {
+      $html = $commentEntry
+      this.$context.trigger('CommentEntry:onSubmitCommentError', { data: $html })
     }
 
-    render(data) {
-        let $html = $(data);
-        let $comment = $html.find(this.selectors.commentBlock);
-        let $title = $html.find(`${this.selectors.responseBlock}-title`);
-        let $commentEntry = $html.find(this.selectors.prefix);
-        let $signIn = $html.find(this.selectors.signInBlock);
-        let $validationError = $html.find(`${this.selectors.prefix}-validationError`);
-        let replaceWithResponse = true;
-
-        // render a comment?
-        if ($comment.length > 0){
-            $html = $comment;
-
-            // if this block is not a child of a commenting block
-            // or is rendered within the replies section, we should replace it with the response
-            replaceWithResponse = this.$context.parents('.Commenting').length == 0 ||
-                                this.$context.parents('.Comment-replies').length > 0;
-
-            this.$context.trigger('CommentEntry:onSubmitCommentSuccess', {
-                commentingId: this.commentingId,
-                $comment: $comment,
-                $title: $title,
-                alreadyRendered: replaceWithResponse
-            });
-
-            this.reset();
-        }
-
-        // render comment entry form?
-        if ($commentEntry.length > 0) {
-            $html = $commentEntry;
-            this.$context.trigger('CommentEntry:onSubmitCommentError', { data: $html });
-        }
-
-        // render sign-in?
-        if ($signIn.length > 0){
-            $html = $signIn;
-            this.$context.trigger('CommentEntry:onSubmitCommentError', { data: $html });
-        }
-
-        // replace the entry block with the comment block?
-        if (replaceWithResponse){
-            this.$context.replaceWith($html);
-        }
+    // render sign-in?
+    if ($signIn.length > 0) {
+      $html = $signIn
+      this.$context.trigger('CommentEntry:onSubmitCommentError', { data: $html })
     }
 
-    reset() {
-        this.$form.get(0).reset();
-        this.$context.find(`${this.selectors.prefix}-validationError`).remove();
-        this.$context.find(`${this.selectors.prefix}-input textarea`).val('').trigger('keyup');
-        this.$context.find(`${this.selectors.validationBlock} [data-visible]`).removeAttr('data-visible');
+    // replace the entry block with the comment block?
+    if (replaceWithResponse) {
+      this.$context.replaceWith($html)
     }
+  }
 
-    remove() {
-        this.$context.remove();
-    }
+  reset () {
+    this.$form.get(0).reset()
+    this.$context.find(`${this.selectors.prefix}-validationError`).remove()
+    this.$context.find(`${this.selectors.prefix}-input textarea`).val('').trigger('keyup')
+    this.$context.find(`${this.selectors.validationBlock} [data-visible]`).removeAttr('data-visible')
+  }
+
+  remove () {
+    this.$context.remove()
+  }
 }
 
 export default bspUtils.plugin(false, 'bsp-community', 'commentEntry', {
-    '_each': function(item) {
-        $(item).data('bsp-community-commentEntry', new CommentEntry($(item), this.option(item)));
-    }
-});
+  '_each': function (item) {
+    $(item).data('bsp-community-commentEntry', new CommentEntry($(item), this.option(item)))
+  }
+})
--- styleguide/block/commenting/Commenting.js
+++ styleguide/block/commenting/Commenting.js
@@ -1,161 +1,161 @@
-import $ from 'jquery';
-import bspUtils from 'bsp-utils';
+import $ from 'jquery'
+import bspUtils from 'bsp-utils'
 
 let Commenting = {
-    defaults: {
-        showComments: false,
+  defaults: {
+    showComments: false,
 
-        selectors: {
-            prefix: ".Commenting",
-            commentEntryBlock: ".CommentEntry",
-            commentBlock: ".Comment",
-            validationBlock: '.ValidationMessages'
-        }
-    },
-
-    init($el, options) {
-        let self = this;
-        self.$el = $el;
-        self.settings = $.extend({}, self.defaults, options);
-        self.id = self.$el.attr('id');
-        self.$commentingBody = self.$el.find(`${self.settings.selectors.prefix}-body`);
-        self.$hideShowCommentsToggles = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-showButton, ${self.settings.selectors.prefix}-hideToggle-hideButton`);
-        self.$showCommentsToggle = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-showButton`);
-        self.$hideCommentsToggle = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-hideButton`);
-        self.$loadMoreForm = self.$el.find(`${self.settings.selectors.prefix}-loadMore`);
-
-        this.$showCommentsToggle.prop('disabled', self.settings.showComments);
-        this.$hideCommentsToggle.prop('disabled', !self.settings.showComments);
-        this.$loadMoreForm.attr('data-visible', self.settings.showComments);
-        this.$commentingBody.attr('data-comments-shown', self.settings.showComments);
-
-        self.$showCommentsToggle.on('click', (event)=> {
-            event.preventDefault();
-            this.showComments();
-        });
-
-        self.$hideCommentsToggle.on('click', (event)=> {
-            event.preventDefault();
-            this.hideComments();
-        });
-
-        self.$loadMoreForm.submit((event)=> {
-            event.preventDefault();
-            self.$el.trigger('Commenting:onRequestLoadMore');
-            this.loadMore();
-        });
-
-        self.$el.on({
-            'CommentEntry:onSubmitCommentSuccess': (event, data)=> {
-                // exit early if this commenting instance shouldn't handle this event.
-                if (!data || self.id !== data.commentingId) return;
-
-                // comment wasn't previously rendered?
-                if (!data.alreadyRendered && data.$comment){
-                    self.renderComment(data.$comment);
-                }
-
-                // update the title?
-                if (data.$title){
-                    self.renderTitle(data.$title);
-                }
-
-                // re-enable any disabled reply buttons
-                self.$el.find('.Comment-reply-button[disabled]').parents(`${self.settings.selectors.commentBlock}`).each(function(){
-                    $(this).data('bsp-community-comment').enableReply();
-                });
-            },
-            'Comment:onRequestReplyUI': (event, data)=> {
-                // exit early if this commenting instance shouldn't handle this event.
-                if (!data || self.id !== data.commentingId) return;
-
-                // re-enable any other disabled reply buttons
-                self.$el.find('.Comment-reply-button[disabled]').parents(`${self.settings.selectors.commentBlock}`).each(function(){
-                    // enable reply buttons other than the one belonging to the current comment
-                    if (!$(event.target).is($(this))) {
-                        $(this).data('bsp-community-comment').enableReply();
-                    }
-                });
-
-                // before a new comment entry block is rendered, remove any existing ones
-                self.$commentingBody.find(`${self.settings.selectors.commentEntryBlock}`).each(function(){
-                    $(this).data('bsp-community-commentEntry').remove();
-                });
-            }
-        });
-    },
-
-    renderTitle($title) {
-        this.$el.find(`${this.settings.selectors.prefix}-title`).html($title.html());
-    },
-
-    renderComment($comment) {
-        this.showComments();
-        this.$commentingBody.prepend($comment);
-    },
-
-    loadMore() {
-        $.ajax({
-            url: this.$loadMoreForm.attr('action'),
-            method: this.$loadMoreForm.attr('method')
-        })
-        .done((response)=> {
-            this.reset();
-            this.renderComments(response);
-            this.$el.trigger('Commenting:onRequestLoadMoreSuccess');
-        })
-        .fail((data)=> {
-            let $errorMessage = this.$el.find(`${this.settings.selectors.prefix}-validation ${this.settings.selectors.validationBlock}-serverError`);
-            $errorMessage.attr('data-visible', '');
-            this.$el.trigger('Commenting:onRequestLoadMoreError', { data: data });
-        });
-    },
-
-    renderComments(data) {
-        let $html = $(data);
-        let $comments = $html.find('.CommentLoadMoreResponse-comments').children(this.settings.selectors.commentBlock);
-        let $loadMoreButton = $html.find('.Commenting-loadMore-button');
-        let formAction = $loadMoreButton.attr('formaction');
-
-        // replace the existing form action with current load more formaction?
-        if (formAction){
-            this.$el.find('.Commenting-loadMore').append($loadMoreButton);
-            $loadMoreButton.parents('form').attr('action', formAction);
-        }
-
-        this.$commentingBody.append($comments);
-    },
-
-    showComments() {
-        this.$el.trigger('Commenting:onBeforeShowComments');
-
-        this.$showCommentsToggle.prop('disabled', true);
-        this.$hideCommentsToggle.prop('disabled', false);
-        this.$commentingBody.attr('data-comments-shown', true);
-        this.$el.find(`${this.settings.selectors.prefix}-loadMore`).attr('data-visible', true);
-
-        this.$el.trigger('Commenting:onAfterShowComments');
-    },
-
-    hideComments() {
-        this.$el.trigger('Commenting:onBeforeHideComments');
-
-        this.$hideCommentsToggle.prop('disabled', true);
-        this.$showCommentsToggle.prop('disabled', false);
-        this.$commentingBody.attr('data-comments-shown', false);
-        this.$el.find(`${this.settings.selectors.prefix}-loadMore`).attr('data-visible', false);
-
-        this.$el.trigger('Commenting:onAfterHideComments');
-    },
-
-    reset() {
-        this.$loadMoreForm.empty();
-        this.$el.find(`${this.settings.selectors.validationBlock} [data-visible]`).removeAttr('data-visible');
+    selectors: {
+      prefix: '.Commenting',
+      commentEntryBlock: '.CommentEntry',
+      commentBlock: '.Comment',
+      validationBlock: '.ValidationMessages'
     }
+  },
+
+  init ($el, options) {
+    let self = this
+    self.$el = $el
+    self.settings = $.extend({}, self.defaults, options)
+    self.id = self.$el.attr('id')
+    self.$commentingBody = self.$el.find(`${self.settings.selectors.prefix}-body`)
+    self.$hideShowCommentsToggles = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-showButton, ${self.settings.selectors.prefix}-hideToggle-hideButton`)
+    self.$showCommentsToggle = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-showButton`)
+    self.$hideCommentsToggle = self.$el.find(`${self.settings.selectors.prefix}-hideToggle-hideButton`)
+    self.$loadMoreForm = self.$el.find(`${self.settings.selectors.prefix}-loadMore`)
+
+    this.$showCommentsToggle.prop('disabled', self.settings.showComments)
+    this.$hideCommentsToggle.prop('disabled', !self.settings.showComments)
+    this.$loadMoreForm.attr('data-visible', self.settings.showComments)
+    this.$commentingBody.attr('data-comments-shown', self.settings.showComments)
+
+    self.$showCommentsToggle.on('click', (event) => {
+      event.preventDefault()
+      this.showComments()
+    })
+
+    self.$hideCommentsToggle.on('click', (event) => {
+      event.preventDefault()
+      this.hideComments()
+    })
+
+    self.$loadMoreForm.submit((event) => {
+      event.preventDefault()
+      self.$el.trigger('Commenting:onRequestLoadMore')
+      this.loadMore()
+    })
+
+    self.$el.on({
+      'CommentEntry:onSubmitCommentSuccess': (event, data) => {
+        // exit early if this commenting instance shouldn't handle this event.
+        if (!data || self.id !== data.commentingId) return
+
+        // comment wasn't previously rendered?
+        if (!data.alreadyRendered && data.$comment) {
+          self.renderComment(data.$comment)
+        }
+
+        // update the title?
+        if (data.$title) {
+          self.renderTitle(data.$title)
+        }
+
+        // re-enable any disabled reply buttons
+        self.$el.find('.Comment-reply-button[disabled]').parents(`${self.settings.selectors.commentBlock}`).each(function () {
+          $(this).data('bsp-community-comment').enableReply()
+        })
+      },
+      'Comment:onRequestReplyUI': (event, data) => {
+        // exit early if this commenting instance shouldn't handle this event.
+        if (!data || self.id !== data.commentingId) return
+
+        // re-enable any other disabled reply buttons
+        self.$el.find('.Comment-reply-button[disabled]').parents(`${self.settings.selectors.commentBlock}`).each(function () {
+          // enable reply buttons other than the one belonging to the current comment
+          if (!$(event.target).is($(this))) {
+            $(this).data('bsp-community-comment').enableReply()
+          }
+        })
+
+        // before a new comment entry block is rendered, remove any existing ones
+        self.$commentingBody.find(`${self.settings.selectors.commentEntryBlock}`).each(function () {
+          $(this).data('bsp-community-commentEntry').remove()
+        })
+      }
+    })
+  },
+
+  renderTitle ($title) {
+    this.$el.find(`${this.settings.selectors.prefix}-title`).html($title.html())
+  },
+
+  renderComment ($comment) {
+    this.showComments()
+    this.$commentingBody.prepend($comment)
+  },
+
+  loadMore () {
+    $.ajax({
+      url: this.$loadMoreForm.attr('action'),
+      method: this.$loadMoreForm.attr('method')
+    })
+        .done((response) => {
+          this.reset()
+          this.renderComments(response)
+          this.$el.trigger('Commenting:onRequestLoadMoreSuccess')
+        })
+        .fail((data) => {
+          let $errorMessage = this.$el.find(`${this.settings.selectors.prefix}-validation ${this.settings.selectors.validationBlock}-serverError`)
+          $errorMessage.attr('data-visible', '')
+          this.$el.trigger('Commenting:onRequestLoadMoreError', { data: data })
+        })
+  },
+
+  renderComments (data) {
+    let $html = $(data)
+    let $comments = $html.find('.CommentLoadMoreResponse-comments').children(this.settings.selectors.commentBlock)
+    let $loadMoreButton = $html.find('.Commenting-loadMore-button')
+    let formAction = $loadMoreButton.attr('formaction')
+
+    // replace the existing form action with current load more formaction?
+    if (formAction) {
+      this.$el.find('.Commenting-loadMore').append($loadMoreButton)
+      $loadMoreButton.parents('form').attr('action', formAction)
+    }
+
+    this.$commentingBody.append($comments)
+  },
+
+  showComments () {
+    this.$el.trigger('Commenting:onBeforeShowComments')
+
+    this.$showCommentsToggle.prop('disabled', true)
+    this.$hideCommentsToggle.prop('disabled', false)
+    this.$commentingBody.attr('data-comments-shown', true)
+    this.$el.find(`${this.settings.selectors.prefix}-loadMore`).attr('data-visible', true)
+
+    this.$el.trigger('Commenting:onAfterShowComments')
+  },
+
+  hideComments () {
+    this.$el.trigger('Commenting:onBeforeHideComments')
+
+    this.$hideCommentsToggle.prop('disabled', true)
+    this.$showCommentsToggle.prop('disabled', false)
+    this.$commentingBody.attr('data-comments-shown', false)
+    this.$el.find(`${this.settings.selectors.prefix}-loadMore`).attr('data-visible', false)
+
+    this.$el.trigger('Commenting:onAfterHideComments')
+  },
+
+  reset () {
+    this.$loadMoreForm.empty()
+    this.$el.find(`${this.settings.selectors.validationBlock} [data-visible]`).removeAttr('data-visible')
+  }
 }
 
 export default bspUtils.plugin(false, 'bsp-community', 'commenting', {
-    '_each': function(item) {
-        Object.create(Commenting).init($(item), this.option(item));
-    }
-});
+  '_each': function (item) {
+    Object.create(Commenting).init($(item), this.option(item))
+  }
+})
