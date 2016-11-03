8b8c3d7200e03b7fc58cd3092de574971aaa75e9
John Pencola <paulpencola@gmail.com>
2016-11-03 16:35:20 -0400

    Add clean-css task to pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
+const gulp = require('gulp')
 const plugins = require('gulp-load-plugins')()
 const autoprefixer = require('autoprefixer')
 const Styleguide = require('brightspot-styleguide')
-const gulp = require('gulp')
 const Builder = require('systemjs-builder')
 
 const styleguide = new Styleguide(gulp)
@@ -11,6 +11,8 @@ gulp.task('css', [ styleguide.task.lint.less() ], () => {
             .pipe(plugins.sourcemaps.init())
             .pipe(plugins.less())
             .pipe(plugins.postcss([ autoprefixer('last 2 versions') ]))
+            .pipe(plugins.cleanCss())
+            .pipe(plugins.rename({ extname: '.min.css' }))
             .pipe(plugins.sourcemaps.write('.'))
             .pipe(gulp.dest(styleguide.path.build()))
             .pipe(styleguide.notify('Finished'))
