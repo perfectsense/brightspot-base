7565ada0e9c6b1b8fe73ea069bb053e4f8c2f379
Dan Slaughter <dan@perfectsensedigital.com>
2016-11-08 19:42:45 -0500

    Fixes package name, and changes method signatures to be strictly typed.

--- core/src/main/java/com/psddev/base/viewmodel/LinkedTextViewModel.java
+++ core/src/main/java/com/psddev/base/viewmodel/LinkedTextViewModel.java
@@ -3,24 +3,24 @@ package com.psddev.base.viewmodel;
 import com.psddev.base.Link;
 import com.psddev.base.LinkTarget;
 import com.psddev.base.LinkedText;
-import com.psddev.base.view.base.util.LinkView;
+import com.psddev.base.view.block.util.LinkView;
 import com.psddev.cms.view.ViewModel;
 
 public class LinkedTextViewModel extends ViewModel<LinkedText> implements LinkView {
 
     @Override
-    public Object getBody() {
+    public String getBody() {
         return model.getTextFinal();
     }
 
     @Override
-    public Object getHref() {
+    public String getHref() {
         Link link = model.getLink();
         return link != null ? link.getLinkUrl() : null;
     }
 
     @Override
-    public Object getTarget() {
+    public String getTarget() {
         Link link = model.getLink();
 
         if (link != null) {
