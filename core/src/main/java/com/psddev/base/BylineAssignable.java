e3a2a425dc05e24e72d6f6b51cafa0d95eb1de23
minkw86 <mk3tb@virginia.edu>
2016-11-15 16:58:21 -0500

    Remove afterCreate method that interferes with main content

--- core/src/main/java/com/psddev/base/BylineAssignable.java
+++ core/src/main/java/com/psddev/base/BylineAssignable.java
@@ -1,16 +1,11 @@
 package com.psddev.base;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import com.psddev.cms.db.ToolUi;
-import com.psddev.cms.db.ToolUser;
-import com.psddev.cms.tool.AuthenticationFilter;
 import com.psddev.dari.db.Modification;
-import com.psddev.dari.db.Query;
 import com.psddev.dari.db.Recordable;
-import com.psddev.dari.util.PageContextFilter;
 
 public interface BylineAssignable extends Recordable {
 
@@ -34,21 +29,6 @@ public interface BylineAssignable extends Recordable {
 
         public void setAuthors(List<Author> authors) {
             this.authors = authors;
-        }
-
-        @Override
-        protected void afterCreate() {
-            super.afterCreate();
-
-            ToolUser user = AuthenticationFilter.Static.getUser(PageContextFilter.Static.getRequest());
-
-            if (user != null) {
-                Author author = Query.from(Author.class).where("name = ?", user.getName()).first();
-
-                if (author != null) {
-                    setAuthors(Arrays.asList(author));
-                }
-            }
         }
     }
 }
