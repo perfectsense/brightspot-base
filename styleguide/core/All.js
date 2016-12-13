38e9505f8e0a4d46d4f4c29870223c0c4a6b00a8
John Pencola <paulpencola@gmail.com>
2016-12-13 11:43:35 -0500

    Removes old comment block

--- styleguide/core/All.js
+++ styleguide/core/All.js
@@ -1,16 +1,3 @@
-/**
- * Our main JS file which is used to compile the JS by including any plugins that are used throughout the project
- * Base has as little JS in it as possible. We try to use reusable JS plugins that have their own repos and can be
- * used across multiple projects. We then compile them in, by bringing them in via bower and they get copied
- * via the bsp-grunt task from their respective repos and compiled in with this file.
- *
- * If you have any project specific JS that you need to write, put it into your own scripts/plugins folder
- * and import from there.
- *
- * If you are going to use any plugins that don't come with base and base components, designate them in your
- * own bower.json and copy them into the scripts/bower folder via a Gruntfile entry
- */
-
 /* eslint-disable no-unused-vars */
 import $ from 'node_modules/jquery/dist/jquery.js'
 import TextInput from './TextInput.js'
