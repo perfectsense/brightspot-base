e514b1c54a5b9d86311aa9c804c9865bdbcbe4f6
Michael Likouris <mlikouris@perfectsensedigital.com>
2016-12-16 16:59:53 -0500

    remove jquery refrence

--- styleguide/community/All.js
+++ styleguide/community/All.js
@@ -1,10 +1,3 @@
-import $ from 'node_modules/jquery/dist/jquery.js'
-
 import Comment from './Comment.js'
 import CommentEntry from './CommentEntry.js'
 import Commenting from './Commenting.js'
-
-
-$(document).ready(function () {
-
-})
