96bcb9869759a1f821cb4008c190da45fa7539d0
John Pencola <paulpencola@gmail.com>
2017-01-03 10:45:40 -0500

    Suppress `no-unused-vars` lint

--- styleguide/community/All.js
+++ styleguide/community/All.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
 import Comment from './Comment.js'
 import CommentEntry from './CommentEntry.js'
 import Commenting from './Commenting.js'
+/* eslint-enable no-unused-vars */
