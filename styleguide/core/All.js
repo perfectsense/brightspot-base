179767acd76236d3a26ad95eceab8de21d8d59d0
John Pencola <paulpencola@gmail.com>
2017-01-06 11:37:03 -0500

    Remove all input components (Input, TextInput, TextAreaInput)

--- styleguide/core/All.js
+++ styleguide/core/All.js
@@ -1,7 +1,5 @@
 /* eslint-disable no-unused-vars */
 import $ from 'node_modules/jquery/dist/jquery.js'
-import TextInput from './TextInput.js'
-import TextAreaInput from './TextAreaInput.js'
 import Gallery from './gallery/Gallery.js'
 import { VideoMain } from './video/VideoMain.js'
 import bspCarouselPlugin from 'bsp-carousel-plugin'
@@ -9,16 +7,6 @@ import bspCarouselPlugin from 'bsp-carousel-plugin'
 
 $(document).ready(function () {
   /* eslint-disable no-new */
-
-  // TextInput
-  $('.TextInput').each((index, value) => {
-    new TextInput($(value))
-  })
-
-  // TextAreaInput
-  $('.TextAreaInput').each((index, value) => {
-    new TextAreaInput($(value))
-  })
 
   // Gallery binding
   $('.Gallery').each((index, value) => {
